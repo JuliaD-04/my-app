dc3757123331cafb636f26cd654eac6be934f420
Julia <air-04@mail.ru>
2020-11-02 13:01:09 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://cdn.pixabay.com/photo/2018/04/05/23/59/coffee-3294633_1280.jpg' />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://sun9-10.userapi.com/c840532/v840532189/3239e/ErM4ry4CQJ4.jpg' />
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                my post
+                <div>
+                    new post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                </div>
+                    <div>
+                        post 2
+                </div>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default Profile;
