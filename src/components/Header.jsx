7d99a66fd484cee83109327d7f56c797b8744df2
Julia <air-04@mail.ru>
2020-11-03 12:59:09 +0300

    14. CSS modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className='header'>
+        <header className= {s.header}>
             <img src='https://cdn.pixabay.com/photo/2018/04/05/23/59/coffee-3294633_1280.jpg' />
         </header>
     );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,36 @@
 import React from 'react';
+import s from'./Navbar.module.css';
+console.log(s)
+
+// Для справки: мы получаем объект у которого есть ключ и значение
+// let s = {
+//   'nav':'Navbar_nav_9YTr',
+//   'item':'Navbar-item_87TR'
+// }
+
+// Если 2 свойства
+// let c1 = 'item';
+// let c2 = 'active';
+// "item active"
+// let classes = c1 + '' + c2
+// let classesNew = `${s.item} ${s.active}`
 
 const Navbar = () => {
   return (
-    <nav className='nav'>
-      <div>
+    <nav className={s.nav}>
+      <div className={s.item}>
         <a>Profile</a>
       </div>
-      <div>
+      <div className={`${s.item} ${s.active}`}>
         <a>Messages</a>
       </div>
-      <div>
+      <div className={s.item}>
         <a>News</a>
       </div>
-      <div>
+      <div className={s.item}>
         <a>Music</a>
       </div>
-      <div>
+      <div className={s.item}>
         <a>Settings</a>
       </div>
     </nav>
