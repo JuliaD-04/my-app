128a7e9aa38d1372bf74e320a371738bd31b5ec2
Julia <air-04@mail.ru>
2020-11-10 15:29:32 +0300

    18. Add dialogs

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import s from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img src='https://sun9-10.userapi.com/c840532/v840532189/3239e/ErM4ry4CQJ4.jpg' />
             </div>
